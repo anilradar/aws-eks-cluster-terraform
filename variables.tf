d92ed91692eee2a449411d74bb6d39f3a4d4befa
anilradar <anilradar099@gmail.com>
2024-01-24 16:50:52 +0530

    adding all the config fiels

---
+++ variables.tf
@@ -0,0 +1,20 @@
+
+variable "subnet_id_1" {
+  type    = string
+  default = "subnet-03461d5323fc1b46d"
+}
+
+variable "subnet_id_2" {
+  type    = string
+  default = "subnet-030e62df9ce8cf43a"
+}
+
+variable "subnet_id_3" {
+  type    = string
+  default = "subnet-030e7f94701b57d72"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
